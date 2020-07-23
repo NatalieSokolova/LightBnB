b1e14f08438a873450dae2376af6a2816e11b989
NatalieSokolova <nataliesklv@gmail.com>
2020-07-23 16:49:09 +0000

    changed addUser to use pool.query

--- LightBnB_WebApp-master/server/database.js
+++ LightBnB_WebApp-master/server/database.js
@@ -48,10 +48,22 @@ exports.getUserWithId = getUserWithId;
  * @return {Promise<{}>} A promise to the user.
  */
 const addUser =  function(user) {
-  const userId = Object.keys(users).length + 1;
-  user.id = userId;
-  users[userId] = user;
-  return Promise.resolve(user);
+  //const [, , name, email, password] = process.argv;
+  const queryString = `INSERT INTO users (name, email, password) VALUES ($1, $2, $3) RETURNING *`;
+  const values = [user.name, user.email, user.password]
+
+  return pool.query(queryString, values)
+  .then(res => res.rows[0])
+
+  // return pool.query(`
+  // INSERT INTO users users (name, email, password) VALUES ($1, $2, $3) RETURNING *
+  // `, [name, email, password])
+  // .then(res => res.rows[0]);
+
+  // const userId = Object.keys(users).length + 1;
+  // user.id = userId;
+  // users[userId] = user;
+  // return Promise.resolve(user);
 }
 exports.addUser = addUser;
 
